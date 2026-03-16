1d4b3e9efc0d865745817d59e46254539d12271d
dwcatron <dwcatron@harrisonschools.com>
2026-03-16 16:10:15 -0400

    Quick fix updating code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -94,9 +94,7 @@ public class RobotContainer {
 
     /* Restored Command Groups */
    /* Restored Command Groups */
-    SequentialCommandGroup Start_Match = new SequentialCommandGroup(
-        new HopperOut(m_hopper, 12)
-    );
+    SequentialCommandGroup Start_Match = new SequentialCommandGroup();
 
     // UPDATED: Using FireFuel with all required arguments and a timeout for Auto
     SequentialCommandGroup Shoot = new SequentialCommandGroup(
